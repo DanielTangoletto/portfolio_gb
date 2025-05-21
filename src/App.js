4cdcf7bbd9657b13cd108bbe468230323b886ce4
Daniel Tangoletto <daniel.tangoletto@live.fr>
2025-05-21 11:31:32 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/portfolio_gb" element={<Homepage />} />
-        <Route path="/portfolio_gb/about" element={<About />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
